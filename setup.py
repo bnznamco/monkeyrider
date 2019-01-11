b9cb0e94bdf083ec413a53a8877e0df62e7e2c4a
bnznamco <gabriele.baldi.01@gmail.com>
2019-01-11 12:48:20 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="monkeyrider",
-    version="0.0.1",
-    author="bnznamco",
-    author_email="gabriele.baldi.01@gmail.com",
-    description="A tool to guide monkeys through unknown apks",
-    url="https://github.com/bnznamco/monkeyrider",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+setup(
+    name='monkeyrider',
+    version='0.0.1',
+    url='https://github.com/bnznamco/monkeyrider',
+    install_requires=[
+        'xmltodict==0.11.0'
     ],
+    description="A tool to guide monkeys through unknown apks",
+    long_description=open('README.md', 'r').read(),
+    license="MIT",
+    author="Gabriele Baldi",
+    author_email="gabriele.baldi.01@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Android Environment',
+        'Framework :: Monkeyrunner',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ]
 )
